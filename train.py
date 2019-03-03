8bfc86bea02892b83e014b3b6a0ed0e11251c574
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2019-03-03 15:31:44 -0500

    MAINt: refactor using DataLoader

--- train.py
+++ train.py
@@ -8,7 +8,7 @@ import torch
 import torch.nn.functional as F
 from matplotlib import pyplot
 from torch import nn, optim
-from torch.utils.data import TensorDataset
+from torch.utils.data import TensorDataset, DataLoader
 
 from constants import PATH, FILENAME
 
@@ -21,10 +21,9 @@ with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
 x_train, y_train, x_valid, y_valid = map(
     torch.tensor, (x_train, y_train, x_valid, y_valid)
 )
-train_ds = TensorDataset(x_train, y_train)
-
-n, c = x_train.shape
 bs = 64
+train_ds = TensorDataset(x_train, y_train)
+train_dl = DataLoader(train_ds, batch_size=bs)
 
 loss_func = F.cross_entropy
 
@@ -48,15 +47,11 @@ def get_model():
     model = MnistLogistic()
     return model, optim.SGD(model.parameters(), lr=lr)
 
-xb, yb = train_ds[0: bs]
-
 model, opt = get_model()
-print("loss: ", loss_func(model(xb), yb))
 
 def fit():
     for epoch in range(epochs):
-        for i in range((n - 1) // bs + 1):
-            xb, yb = train_ds[i*bs: i*bs+bs]
+        for xb, yb in train_dl:
             pred = model(xb)
             loss = loss_func(pred, yb)
 
@@ -66,5 +61,6 @@ def fit():
 
 fit()
 
+xb, yb = x_train[0: bs], y_train[0: bs]
 print("loss: ", loss_func(model(xb), yb))
 print("accuracy: ", accuracy(model(xb), yb))
