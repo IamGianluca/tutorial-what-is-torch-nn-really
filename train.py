22e7a223d637eec0747c6d6a7ac2148db3ed338a
Gianluca Rossi <gr.gianlucarossi@gmail.com>
2019-03-03 15:59:59 -0500

    ENH: create fit() and get_data()

--- train.py
+++ train.py
@@ -21,17 +21,9 @@ with gzip.open((PATH / FILENAME).as_posix(), "rb") as f:
 x_train, y_train, x_valid, y_valid = map(
     torch.tensor, (x_train, y_train, x_valid, y_valid)
 )
-bs = 64
 train_ds = TensorDataset(x_train, y_train)
-train_dl = DataLoader(train_ds, batch_size=bs, shuffle=True)
-
-# NOTE: we’ll use a batch size for the validation set that is twice as large
-# as that for the training set. This is because the validation set does not
-# need backpropagation and thus takes less memory
 valid_ds = TensorDataset(x_valid, y_valid)
-valid_dl = DataLoader(valid_ds, batch_size=bs * 2)
 
-loss_func = F.cross_entropy
 
 def accuracy(out, yb):
     preds = torch.argmax(out, dim=1)
@@ -45,37 +37,45 @@ class MnistLogistic(nn.Module):
     def forward(self, xb):
         return self.lin(xb)
 
-model = MnistLogistic()
-lr = 0.5
-epochs = 2
-
 def get_model():
     model = MnistLogistic()
     return model, optim.SGD(model.parameters(), lr=lr)
 
-model, opt = get_model()
-xb, yb = x_train[0: bs], y_train[0: bs]
-print("loss: ", loss_func(model(xb), yb))
+def get_data(train_ds, valid_ds, bs):
+    return (
+        DataLoader(train_ds, batch_size=bs, shuffle=True),
+        DataLoader(valid_ds, batch_size=bs * 2),
+    )
 
-def fit():
+def loss_batch(model, loss_func, xb, yb, opt=None):
+    loss = loss_func(model(xb), yb)
+
+    if opt is not None:
+        loss.backward()
+        opt.step()
+        opt.zero_grad()
+
+    return loss.item(), len(xb)
+
+def fit(epochs, model, loss_func, opt, train_dl, valid_dl):
     for epoch in range(epochs):
         model.train()
         for xb, yb in train_dl:
-            pred = model(xb)
-            loss = loss_func(pred, yb)
-
-            loss.backward()
-            opt.step()
-            opt.zero_grad()
+            loss_batch(model, loss_func, xb, yb, opt)
 
         model.eval()
         with torch.no_grad():
-            valid_loss = sum(loss_func(model(xb), yb) for xb, yb in valid_dl)
+            losses, nums = zip(
+                *[loss_batch(model, loss_func, xb, yb) for xb, yb in valid_dl])
+            val_loss = np.sum(np.multiply(losses, nums)) / np.sum(nums)
 
-        print(epoch, valid_loss / len(valid_dl))
+        print(epoch, val_loss)
 
-fit()
+lr = 0.05
+epochs = 20
+bs = 64
+loss_func = F.cross_entropy
 
-xb, yb = x_train[0: bs], y_train[0: bs]
-print("loss: ", loss_func(model(xb), yb))
-# print("accuracy: ", accuracy(model(xb), yb))
+train_dl, valid_dl = get_data(train_ds, valid_ds, bs)
+model, opt = get_model()
+fit(epochs, model, loss_func, opt, train_dl, valid_dl)
